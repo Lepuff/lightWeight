cda7dab5b2d9010c6ded59fd5fb25b1c06e140bf
Lepuff <48945554+Lepuff@users.noreply.github.com>
2020-02-03 12:05:47 +0100

    init

---
+++ app/src/main/java/com/example/lightweight/Navigation.kt
@@ -0,0 +1,12 @@
+package com.example.lightweight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Navigation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}

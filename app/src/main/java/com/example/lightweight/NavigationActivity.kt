be66ddb8ca2bf3228b3ea7e4205f4d55d1478a18
Lepuff <48945554+Lepuff@users.noreply.github.com>
2020-02-17 09:29:38 +0100

    init

--- app/src/main/java/com/example/lightweight/NavigationActivity.kt
+++ app/src/main/java/com/example/lightweight/NavigationActivity.kt
@@ -1,76 +1,29 @@
 package com.example.lightweight
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.Menu
-import android.widget.Toolbar
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_navigation.*
-
 class NavigationActivity : AppCompatActivity() {
-
-
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-
-
-
-
-
-
-
-
-
-
-        loadFragment(FeedFragment())
-        bottomNav.setOnNavigationItemSelectedListener {
-            when (it.itemId){
-                R.id.nav_analytics ->{
-                loadFragment(AnalyticsFragment())
-                    return@setOnNavigationItemSelectedListener true
-
-                }
-                R.id.nav_feed -> {
-                    loadFragment(FeedFragment())
-                    return@setOnNavigationItemSelectedListener true
-                }
-                R.id.nav_social -> {
-                    loadFragment(SocialFragment())
-                    return@setOnNavigationItemSelectedListener true
-                }
-
-            }
-            false
-
-        }
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
-    private fun loadFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
-
 }
---
+++ app/src/main/java/com/example/lightweight/ui/Settings/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.lightweight.ui.Settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

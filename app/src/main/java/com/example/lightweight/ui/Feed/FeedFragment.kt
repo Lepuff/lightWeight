bbbf919abdad5fc8c201db2f0f1d9c30844601a9
Lepuff <48945554+Lepuff@users.noreply.github.com>
2020-02-19 12:15:24 +0100

    FAB complete

--- app/src/main/java/com/example/lightweight/ui/Feed/FeedFragment.kt
+++ app/src/main/java/com/example/lightweight/ui/Feed/FeedFragment.kt
@@ -1,17 +1,24 @@
 package com.example.lightweight.ui.Feed
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lightweight.DataSource
 import com.example.lightweight.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_feed.*
+import kotlinx.android.synthetic.main.fragment_feed.view.*
 
 class FeedFragment : Fragment() {
 
@@ -25,10 +32,18 @@ class FeedFragment : Fragment() {
         feedViewModel =
             ViewModelProviders.of(this).get(FeedViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_feed, container, false)
+        val floatingActionBar =
+            root.findViewById<FloatingActionButton>(R.id.feed_floating_action_button)
+
+
+
 
         feedViewModel.text.observe(this, Observer {
 
         })
+        floatingActionBar.setOnClickListener {
+            chooseWorkoutDialog(root)
+        }
 
         return root
     }
@@ -50,9 +65,40 @@ class FeedFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+
+
         initRecyclerView()
         addDataSet()
 
+
     }
 
+    private fun chooseWorkoutDialog(view: View) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(view.context)
+        val workOutType = arrayOf("Gym", "Cycling", "Running")
+        builder
+            .setTitle("test")
+            .setItems(workOutType) { dialog, which ->
+                when (which) {
+                    
+                    0 -> {
+
+                    }
+                    1 -> {
+
+                    }
+                    2 -> {
+
+                    }
+
+
+                }
+
+            }
+
+
+            .show()
+
+    }
 }

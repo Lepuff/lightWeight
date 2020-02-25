4f68465a1dfe259fb466b008626214259b39119f
Samuel <48945979+svsa1721@users.noreply.github.com>
2020-02-25 01:01:17 +0100

    02/25

--- app/src/main/java/com/example/lightweight/Database.kt
+++ app/src/main/java/com/example/lightweight/Database.kt
@@ -2,6 +2,9 @@ package com.example.lightweight
 
 import com.facebook.AccessToken
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import java.time.LocalDateTime
+import java.util.*
 
 object Database{
     fun updateUserData(accessToken: AccessToken, firstName: String, lastName: String, email: String){
@@ -15,5 +18,24 @@ object Database{
         // Add a new document with a email-adress as ID
         db.collection("users").document(email)
             .set(user)
+    }
+
+    fun addWorkout(accessToken: AccessToken, email: String){
+        val db = FirebaseFirestore.getInstance()
+
+        val weight: Int? = null
+        val reps: Int? = null
+        val sets: Int? = null
+        val date: LocalDateTime = LocalDateTime.now()
+        val typeOfExercise: String? = null
+
+        val gymExercise = hashMapOf(
+            "reps" to reps,
+            "sets" to sets,
+            "weight" to weight
+        )
+
+        db.collection("users").document(email)
+            .collection("workouts").document(date.toString() + typeOfExercise)
     }
 }

4f7c07b04c87321e21c11d1cc6bf23295016d816
Arvin <taar18le@student.ju.se>
2020-02-27 14:41:02 +0100

    temporary fragcode

--- app/src/main/java/com/example/lightweight/ui/Social/SocialFragment.kt
+++ app/src/main/java/com/example/lightweight/ui/Social/SocialFragment.kt
@@ -8,7 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.lightweight.DataSource
 import com.example.lightweight.R
+import com.example.lightweight.ui.Feed.TopSpacingItemDecoration
+import com.example.lightweight.ui.Feed.WorkOutAdapter
+import kotlinx.android.synthetic.main.fragment_feed.*
 
 class SocialFragment : Fragment() {
 
@@ -29,5 +34,25 @@ class SocialFragment : Fragment() {
         })
 
         return root
+    }
+
+    private fun addDataSet(){
+        val data = DataSource.createDataSet()
+        socialAdapter.submitList(data)
+    }
+
+
+    private fun initRecyclerView() {
+        feed_recycler_view.apply {
+            layoutManager = LinearLayoutManager(this.context)
+            val topSpacingItemDecoration = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingItemDecoration)
+            socialAdapter = SocialAdapter()
+            adapter = socialAdapter
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
     }
 }

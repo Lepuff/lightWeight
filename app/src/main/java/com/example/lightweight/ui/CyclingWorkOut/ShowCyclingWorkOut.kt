c86b243b2397678005e0f3e31435a3e17861e37d
Arvin <taar18le@student.ju.se>
2020-03-03 17:02:57 +0100

    afternav

---
+++ app/src/main/java/com/example/lightweight/ui/CyclingWorkOut/ShowCyclingWorkOut.kt
@@ -0,0 +1,12 @@
+package com.example.lightweight.ui.CyclingWorkOut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowCyclingWorkOut : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_cycling_work_out)
+    }
+}

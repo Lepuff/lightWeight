3d9a9d56bfd755d662e9541aa45fb6b686988723
Arvin <taar18le@student.ju.se>
2020-03-03 22:35:20 +0100

    cycle fort.

--- app/src/main/java/com/example/lightweight/ui/CyclingWorkOut/ShowCyclingWorkOut.kt
+++ app/src/main/java/com/example/lightweight/ui/CyclingWorkOut/ShowCyclingWorkOut.kt
@@ -2,6 +2,7 @@ package com.example.lightweight.ui.CyclingWorkOut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lightweight.R
 
 class ShowCyclingWorkOut : AppCompatActivity() {
 

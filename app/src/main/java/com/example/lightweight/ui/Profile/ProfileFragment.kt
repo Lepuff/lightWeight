f734dbe16e55735620ebce24f0d02cdb6a1e8228
Lepuff <oskar11.person@gmail.com>
2020-03-19 13:54:48 +0100

    bugfix

--- app/src/main/java/com/example/lightweight/ui/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/lightweight/ui/Profile/ProfileFragment.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.app.AlertDialog
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -167,6 +168,7 @@ class ProfileFragment : Fragment() {
             .fallback(R.drawable.ic_person_yellow_24dp)
 
 
+        Log.d("image Uri value::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ",imageUri.toString())
         Glide.with(this)
             .applyDefaultRequestOptions(requestOption)
             .load(imageUri)

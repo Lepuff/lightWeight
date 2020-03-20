db76c1b1746c1ef9f11f84d2a74cc55b296a3791
Samuel <48945979+svsa1721@users.noreply.github.com>
2020-03-20 21:01:41 +0100

    request_storage_permission_profile_frag

--- app/src/main/java/com/example/lightweight/ui/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/lightweight/ui/Profile/ProfileFragment.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,7 +17,8 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.net.toUri
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -59,7 +61,7 @@ class ProfileFragment : Fragment() {
         viewModel =
             ViewModelProviders.of(this).get(ProfileViewModel::class.java)
         viewModel.isInEditState.observe(viewLifecycleOwner, Observer {
-            handelEditStates()
+            handleEditStates()
         })
         loadUserInfo(root)
 
@@ -73,7 +75,7 @@ class ProfileFragment : Fragment() {
         val cameraButton =
             root.findViewById<ImageButton>(R.id.profile_camera_button)
         cameraButton.setOnClickListener {
-            pickPhotoFromGallery()
+                requestStoragePermission()
         }
 
         val addFriendsButton = root.findViewById<Button>(R.id.profile_add_friends_button)
@@ -100,6 +102,29 @@ class ProfileFragment : Fragment() {
         }
         return root
 
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PICK_PHOTO_REQUEST){
+            if (permissions[0].equals(android.Manifest.permission.READ_EXTERNAL_STORAGE) && grantResults[0]
+            == PackageManager.PERMISSION_GRANTED){
+                pickPhotoFromGallery()
+            }
+        }
+    }
+
+    private fun requestStoragePermission(){
+        if (ActivityCompat.checkSelfPermission(context!!, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), PICK_PHOTO_REQUEST)
+        } else{
+            pickPhotoFromGallery()
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -130,7 +155,6 @@ class ProfileFragment : Fragment() {
         }
 
     }
-
 
     private fun saveNewPassword(dialogView: View, dialog: AlertDialog) {
         val currentUser = FirebaseAuth.getInstance().currentUser!!
@@ -182,6 +206,7 @@ class ProfileFragment : Fragment() {
             }
         }
     }
+
 
     private fun saveProfileInfo(view: View) {
         val firstName =
@@ -249,7 +274,7 @@ class ProfileFragment : Fragment() {
         }
     }
 
-    private fun handelEditStates() {
+    private fun handleEditStates() {
         if (Database.isFacebookUser()) {
             requireView().findViewById<Button>(R.id.profile_change_password_button).visibility =
                 View.GONE

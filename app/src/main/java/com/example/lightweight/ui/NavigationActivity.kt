8c50e4197d8c4436e354840d2f6b2e8216d541ec
Lepuff <oskar11.person@gmail.com>
2020-03-20 16:25:38 +0100

    navbar bugfix

--- app/src/main/java/com/example/lightweight/ui/NavigationActivity.kt
+++ app/src/main/java/com/example/lightweight/ui/NavigationActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -22,7 +23,9 @@ class NavigationActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
 
-        val navController = findNavController(R.id.nav_host_fragment)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_profile,
